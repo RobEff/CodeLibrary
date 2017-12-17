d563249f32e2e8d6dd9ef2a7cbc1b507932684f6
RobEff <34625045+RobEff@users.noreply.github.com>
2017-12-17 10:34:59 -0500

    Creation of new Hello World application.

---
+++ Console App Hello World/Console App Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_App_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
